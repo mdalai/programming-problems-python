6ea1e0c0ae7a38f01f149e66f4abe89f96f26239
Dalai <mingat.dalai@gmail.com>
2017-06-14 21:09:02 -0500

    add binaryTree

---
+++ binaryTree.py
@@ -0,0 +1,63 @@
+class  BinaryTree:
+	def __init__(self,rootObj):
+		self.key = rootObj
+		self.leftChild = None
+		self.rightChild = None
+
+	def insertLeft(self, newNode):
+		if self.leftChild ==None:
+			self.leftChild = BinaryTree(newNode)
+		else:
+			t = BinaryTree(newNode)
+			t.leftChild = self.leftChild
+			self.leftChild = t
+
+	def insertRight(self, newNode):
+		if self.rightChild == None:
+			self.rightChild = BinaryTree(newNode)
+		else:
+			t = BinaryTree(newNode)
+			t.rightChild = self.rightChild
+			self.rightChild = t
+	
+	def getRightChild(self):
+		return self.rightChild
+
+	def getLeftChild(self):
+		return self.leftChild
+
+	def setRootVal(self, obj):
+		self.key = obj
+
+	def getRootVal(self):
+		return self.key
+
+r = BinaryTree('a')
+print(r.getRootVal())
+print(r.getLeftChild())
+r.insertLeft('b')
+print(r.getLeftChild())
+print(r.getLeftChild().getRootVal())
+r.insertRight('c')
+print(r.getRightChild())
+print(r.getRightChild().getRootVal())
+r.getRightChild().setRootVal('hello')
+print(r.getRightChild().getRootVal())
+
+
+def buildTree():
+    r = BinaryTree('a')
+    r.insertLeft('b')
+    r.getLeftChild().insertRight('d')
+    r.insertRight('c')
+    r.getRightChild().insertLeft('e')
+    r.getRightChild().insertRight('f')
+
+    return r
+
+ttree = buildTree()
+print(ttree.getLeftChild().getRootVal())
+print(ttree.getLeftChild().getRightChild().getRootVal())
+print(ttree.getRightChild().getRootVal())
+print(ttree.getRightChild().getLeftChild().getRootVal())
+print(ttree.getRightChild().getRightChild().getRootVal())
